cf2d334364436f33414380732c58e11ff7c10685
JustKode <sobu0715@gmail.com>
2023-01-30 00:47:36 +0900

    feat: hive

--- main.sh
+++ main.sh
@@ -13,3 +13,4 @@ docker build -t hadoop-namenode:3.3.0 ./namenode
 docker build -t hadoop-resource-manager:3.3.0 ./resource-manager
 docker build -t yarn-timeline-server:3.3.0 ./yarn-timeline-server
 docker build -t hadoop-datanode:3.3.0 ./datanode
+docker build -t hive-image:3.1.2 ./hive-image
