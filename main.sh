7e9e838e87f2d1ac80b3b5159cffde21fd20e200
JustKode <sobu0715@gmail.com>
2023-02-02 01:43:00 +0900

    fix: build script

--- main.sh
+++ main.sh
@@ -9,12 +9,12 @@ else
     docker build -t hadoop-base-image:3.3.0 ./hadoop-base-image-amd
 fi
 
-docker build -t spark-base-image:3.3.1 ./spark-base-image
+docker build -t spark-base-image:3.1.1 ./spark-base-image
 
-docker build -t spark-history-server:3.3.1 ./spark-history-server
+docker build -t spark-history-server:3.1.1 ./spark-history-server
 docker build -t hadoop-namenode:3.3.0 ./namenode
 docker build -t hadoop-resource-manager:3.3.0 ./resource-manager
-docker build -t yarn-timeline-server:3.3.0 ./yarn-timeline-server
+docker build -t yarn-timeline-server:3.1.1 ./yarn-timeline-server
 docker build -t hadoop-datanode:3.3.0 ./datanode
 docker build -t hive-image:3.1.2 ./hive-image
 docker build -t zeppelin:0.10.1 ./zeppelin
